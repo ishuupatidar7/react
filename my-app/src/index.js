0505281e4be3abf22670b5513a7660589caf4248
ishuupatidar7 <92732233+ishuupatidar7@users.noreply.github.com>
2024-09-16 16:13:28 +0530

    Update index.js
    IP# direct commit from main branch for make conflict

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -12,6 +12,7 @@ root.render(
 );
 // THis is direct commit in master branch
 // If you want to start measuring performance in your app, pass a function
+//now this is direct commit from main branch for make conflict
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // Now this is new comment in new-feature branch
