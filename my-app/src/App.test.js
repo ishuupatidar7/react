3620c5222db8220700b508a3955d6526876c807f
ISHWAR PATIDAR <ishwar-lal.patidar@eviden.com>
2024-09-16 21:23:28 +0530

    IP# Third commit in feature-1234 in test case

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  //new line added in the test case in feature-1234
   const linkElement = screen.getByText(/learn react/i);
   //New test added in new-feature branch
   expect(linkElement).toBeInTheDocument();
