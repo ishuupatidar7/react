80612bee17dd43a7bcfa29ef9dfd262864957c79
ISHWAR PATIDAR <ishwar-lal.patidar@eviden.com>
2024-09-16 14:39:48 +0530

    IP# new para1 added in feature

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>This is demo project</p>
+        <p>new para added in feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
