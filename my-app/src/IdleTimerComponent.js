9720a2b6c73b249b3707383ca96e4dc3dbf5aaae
ISHWAR PATIDAR <ishwar-lal.patidar@eviden.com>
2024-09-18 20:54:39 +0530

    IP# CODE 2 added in feature-2222

--- my-app/src/IdleTimerComponent.js
+++ my-app/src/IdleTimerComponent.js
@@ -57,6 +57,12 @@ function IdleTimerComponent() {
     return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
   };
 
+
+  const formatTimecode2 = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
+  };
   return (
     <div>
       {isIdle && (
