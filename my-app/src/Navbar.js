b15ecce81cf992c9ee73e792b7732f59c5db84ad
ISHWAR PATIDAR <ishwar-lal.patidar@eviden.com>
2024-09-24 12:04:03 +0530

    IP# new code added in navbarfile in MAIN branch

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -8,6 +8,9 @@ function Navbar() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
        
       </header>
